488ed234b7dde2f08e25ef0e8049273b303110bc
Thanh Tung <tung212t@gmail.com>
2024-02-14 11:09:07 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you!! ;))</div>
+          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you!!</div>
         </>
       ) : (
         <>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/will-you-be-my-valentine",
+  base: "/VLT_ONE",
   plugins: [react()],
+  build: {
+    sourcemap: true, // hoặc false tùy thuộc vào nhu cầu của bạn
+  },
 });
